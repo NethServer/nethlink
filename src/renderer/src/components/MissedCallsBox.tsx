439d7732b800f8908144446c3a8418574093548f
edoardopaolinelli <edoardo.paolinelli@gmail.com>
2024-02-19 15:33:31 +0100

    feat: add simple data for testing and change some graphics rendering logic

--- src/renderer/src/components/MissedCallsBox.tsx
+++ src/renderer/src/components/MissedCallsBox.tsx
@@ -13,14 +13,29 @@ export interface MissedCallsBoxProps {
 }
 
 export function MissedCallsBox({ title, label, onClick }: MissedCallsBoxProps): JSX.Element {
-
   const [missedCalls, setMissedCalls] = useState<CallData[]>([])
   useInitialize(() => {
     window.api.onReciveLastCalls(saveMissedCalls)
+
+    //TODO da guardare come passare la tipologia di MissedCall, tipo commercial, customer care...
+    saveMissedCalls({
+      count: 3,
+      rows: [
+        { cnam: 'Tanya Fox', cnum: '530', duration: 1, time: 14 },
+        { cnam: 'Unknown', cnum: '333 756 0091', duration: 10, time: 12, ccompany: 'Commercial' },
+        {
+          cnam: 'Maple office customer service',
+          cnum: '02 3456785',
+          duration: 10,
+          time: 12,
+          ccompany: 'Customer Care'
+        }
+      ]
+    })
   })
 
   async function saveMissedCalls(historyResponse: HistoryCallData) {
-    setMissedCalls(historyResponse.rows)
+    setMissedCalls(() => historyResponse.rows)
   }
 
   return (
@@ -35,11 +50,20 @@ export function MissedCallsBox({ title, label, onClick }: MissedCallsBoxProps): 
           <p className="text-blue-500">{label}</p>
         </Button>
       </div>
-      <div className="flex flex-col gap-2 p-2 min-h-[240px] overflow-y-auto">
-        {missedCalls?.map((call) => {
+      <div className="flex flex-col gap-2 p-2 min-h-[240px]">
+        {missedCalls?.map((call, idx) => {
           return (
-            <div className="border-b pb-2 border-gray-700">
-              <MissedCall name={call.cnam} number={call.cnum} time={call.time} />
+            <div
+              className={`${idx === missedCalls.length - 1 ? `` : `border-b pb-2 border-gray-700`}`}
+              key={idx}
+            >
+              <MissedCall
+                name={call.cnam!}
+                number={call.cnum!}
+                time={call.time!}
+                duration={call.duration!}
+                company={call.ccompany!}
+              />
             </div>
           )
         })}
@@ -47,5 +71,3 @@ export function MissedCallsBox({ title, label, onClick }: MissedCallsBoxProps): 
     </div>
   )
 }
-
-
