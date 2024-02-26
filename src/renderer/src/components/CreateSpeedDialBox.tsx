0f38f73712e9fd41eb06971b80bb7f2aa43d8090
edoardopaolinelli <edoardo.paolinelli@gmail.com>
2024-02-26 12:51:51 +0100

    create a separated componente called CreateSpeedDialBox

---
+++ src/renderer/src/components/CreateSpeedDialBox.tsx
@@ -0,0 +1,85 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Button, TextInput } from './Nethesis'
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons'
+import { useRef, useState } from 'react'
+
+export interface CreateSpeedDialProps {
+  setIsCreatingSpeedDial: (isCreating: boolean) => void
+  setIsAddedSuccessfully: (isAdded: boolean) => void
+}
+
+export function CreateSpeedDialBox({
+  setIsCreatingSpeedDial,
+  setIsAddedSuccessfully
+}: CreateSpeedDialProps) {
+  const [name, setName] = useState<string>('')
+  const [phoneNumber, setPhoneNumber] = useState<string>('')
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const nameRef = useRef<HTMLInputElement>(null)
+  const phoneNumberRef = useRef<HTMLInputElement>(null)
+
+  /* TODO da aggiungere una chiamata alle api in cui si aggiunge la nuova SpeedDial */
+  async function handleCreateSpeedDial() {
+    setIsLoading(true)
+    console.log('Creating speed dial', { name, phoneNumber })
+    setTimeout(() => {
+      setIsAddedSuccessfully(true)
+      setIsLoading(false)
+      setIsCreatingSpeedDial(false)
+      setName('')
+      setPhoneNumber('')
+    }, 2000)
+  }
+
+  return (
+    <>
+      <div className="flex justify-between items-center py-1 border border-t-0 border-r-0 border-l-0 border-gray-700 max-h-[28px]">
+        <h1 className="font-semibold">Create speed dial</h1>
+      </div>
+      <TextInput
+        ref={nameRef}
+        type="text"
+        label="Name"
+        value={name}
+        onChange={(e) => {
+          nameRef.current!.value = e.target.value
+          setName(() => nameRef.current!.value)
+        }}
+      />
+      <TextInput
+        ref={phoneNumberRef}
+        type="tel"
+        label="Phone number"
+        pattern="[0-9]*"
+        minLength={3}
+        value={phoneNumber}
+        onChange={(e) => {
+          const value = e.target.value
+          const cleanedValue = value.replace(/\D/g, '')
+
+          if (phoneNumberRef.current) {
+            phoneNumberRef.current.value = cleanedValue
+          }
+          setPhoneNumber(cleanedValue)
+        }}
+      />
+      <div className="flex flex-row gap-4 justify-end mt-2">
+        <Button
+          variant="ghost"
+          className="text-blue-500"
+          onClick={() => setIsCreatingSpeedDial(false)}
+        >
+          <p className="text-blue-500 font-semibold">Cancel</p>
+        </Button>
+        <Button
+          className="bg-blue-500 gap-3"
+          disabled={name.trim().length === 0 || phoneNumber.trim().length < 3}
+          onClick={handleCreateSpeedDial}
+        >
+          <p className="text-black font-semibold">Create</p>
+          {isLoading && <FontAwesomeIcon icon={faCircleNotch} className="text-black" spin />}
+        </Button>
+      </div>
+    </>
+  )
+}
--- src/renderer/src/components/SpeedDialsBox.tsx
+++ src/renderer/src/components/SpeedDialsBox.tsx
@@ -5,6 +5,7 @@ import { SpeedDialType } from '@shared/types'
 import { Button } from './Nethesis/Button'
 import { useRef, useState } from 'react'
 import { TextInput } from './Nethesis'
+import { CreateSpeedDialBox } from './CreateSpeedDialBox'
 
 export interface SpeedDialsBoxProps {
   speeddials: SpeedDialType[] | undefined
@@ -14,30 +15,7 @@ export interface SpeedDialsBoxProps {
 
 export function SpeedDialsBox({ speeddials, label, callUser }: SpeedDialsBoxProps): JSX.Element {
   const [isCreatingSpeedDial, setIsCreatingSpeedDial] = useState<boolean>(false)
-  const [isLoading, setIsLoading] = useState<boolean>(false)
   const [isAddedSuccessfully, setIsAddedSuccessfully] = useState<boolean>(false)
-  const [name, setName] = useState<string>('')
-  const [phoneNumber, setPhoneNumber] = useState<string>('')
-  const nameRef = useRef<HTMLInputElement>(null)
-  const phoneNumberRef = useRef<HTMLInputElement>(null)
-
-  /* TODO da aggiungere una chiamata alle api in cui si aggiunge la nuova SpeedDial */
-  async function handleCreateSpeedDial() {
-    setIsLoading(true)
-    try {
-      setTimeout(() => {
-        console.log('Speed dial aggiunto', { name, phoneNumber })
-        setIsAddedSuccessfully(true)
-        setIsLoading(false)
-        setName('')
-        setPhoneNumber('')
-        setIsCreatingSpeedDial(false)
-      }, 2000)
-    } catch (error) {
-      console.error("Errore durante l'aggiunta del speed dial", error)
-      setIsLoading(false)
-    }
-  }
 
   return (
     <div className="flex flex-col gap-4 min-h-[284px]">
@@ -48,55 +26,10 @@ export function SpeedDialsBox({ speeddials, label, callUser }: SpeedDialsBoxProp
         </div>
       )}
       {isCreatingSpeedDial ? (
-        <>
-          <div className="flex justify-between items-center py-1 border border-t-0 border-r-0 border-l-0 border-gray-700 max-h-[28px]">
-            <h1 className="font-semibold">Create speed dial</h1>
-          </div>
-          <TextInput
-            ref={nameRef}
-            type="text"
-            label="Name"
-            value={name}
-            onChange={(e) => {
-              nameRef.current!.value = e.target.value
-              setName(() => nameRef.current!.value)
-            }}
-          />
-          <TextInput
-            ref={phoneNumberRef}
-            type="tel"
-            label="Phone number"
-            pattern="[0-9]*"
-            minLength={3}
-            value={phoneNumber}
-            onChange={(e) => {
-              const value = e.target.value
-              const cleanedValue = value.replace(/\D/g, '')
-
-              if (phoneNumberRef.current) {
-                phoneNumberRef.current.value = cleanedValue
-              }
-              setPhoneNumber(cleanedValue)
-            }}
-          />
-          <div className="flex flex-row gap-4 justify-end mt-2">
-            <Button
-              variant="ghost"
-              className="text-blue-500"
-              onClick={() => setIsCreatingSpeedDial(false)}
-            >
-              <p className="text-blue-500 font-semibold">Cancel</p>
-            </Button>
-            <Button
-              className="bg-blue-500 gap-3"
-              disabled={name.trim().length === 0 || phoneNumber.trim().length < 3}
-              onClick={handleCreateSpeedDial}
-            >
-              <p className="text-black font-semibold">Create</p>
-              {isLoading && <FontAwesomeIcon icon={faCircleNotch} className="text-black" spin />}
-            </Button>
-          </div>
-        </>
+        <CreateSpeedDialBox
+          setIsCreatingSpeedDial={setIsCreatingSpeedDial}
+          setIsAddedSuccessfully={setIsAddedSuccessfully}
+        />
       ) : (
         <>
           <div className="flex justify-between items-center py-1 border border-t-0 border-r-0 border-l-0 border-gray-700 font-semibold max-h-[28px]">
