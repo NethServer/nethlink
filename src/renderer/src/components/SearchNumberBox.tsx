7498a45e0721a7b82a37aaae5d7210c1c1c7bab6
edoardopaolinelli <edoardo.paolinelli@gmail.com>
2024-02-29 15:19:28 +0100

    delete the AddToPhoneBook from inside this component

--- src/renderer/src/components/SearchNumberBox.tsx
+++ src/renderer/src/components/SearchNumberBox.tsx
@@ -4,21 +4,16 @@ import { SearchNumber } from './SearchNumber'
 import { useInitialize } from '@renderer/hooks/useInitialize'
 import { useState } from 'react'
 import { SearchCallData, SearchData } from '@shared/types'
-import { AddToPhonebookBox } from './AddToPhonebookBox'
 
 export interface SearchNumberBoxProps {
   searchText: string
   callUser: (phoneNumber: string) => void
-  handleReset: () => void
-  isAddingToPhonebook: boolean
   handleAddContactToPhonebook: (boolean) => void
 }
 
 export function SearchNumberBox({
   searchText,
   callUser,
-  handleReset,
-  isAddingToPhonebook,
   handleAddContactToPhonebook
 }: SearchNumberBoxProps) {
   const [filteredPhoneNumbers, setFilteredPhoneNumbers] = useState<SearchData[]>([])
@@ -32,47 +27,34 @@ export function SearchNumberBox({
   }
 
   return (
-    <>
-      {isAddingToPhonebook ? (
-        <AddToPhonebookBox
-          searchText={searchText}
-          handleAddContactToPhonebook={handleAddContactToPhonebook}
-          handleReset={handleReset}
-        />
-      ) : (
-        <div className="flex flex-col dark:text-gray-50 text-gray-900 dark:bg-gray-900 bg-gray-50">
-          <div
-            className="flex gap-5 pt-[10px] pr-8 pb-[10px] pl-7 min-h-9 items-start dark:hover:bg-gray-700 hover:bg-gray-200 cursor-pointer"
-            onClick={() => callUser(searchText)}
-          >
-            <FontAwesomeIcon className="text-base dark:text-gray-50 text-gray-600" icon={faPhone} />
-            <p className="font-semibold">Call {searchText}</p>
-          </div>
+    <div className="flex flex-col dark:text-gray-50 text-gray-900 dark:bg-gray-900 bg-gray-50">
+      <div
+        className="flex gap-5 pt-[10px] pr-8 pb-[10px] pl-7 min-h-9 items-start dark:hover:bg-gray-700 hover:bg-gray-200 cursor-pointer"
+        onClick={() => callUser(searchText)}
+      >
+        <FontAwesomeIcon className="text-base dark:text-gray-50 text-gray-600" icon={faPhone} />
+        <p className="font-semibold">Call {searchText}</p>
+      </div>
 
-          <div
-            className="flex gap-5 pt-[10px] pr-8 pb-[10px] pl-7 w-full min-h-9 dark:hover:bg-gray-700 hover:bg-gray-200 cursor-pointer"
-            onClick={() => handleAddContactToPhonebook(true)}
-          >
-            <FontAwesomeIcon
-              className="text-base dark:text-gray-50 text-gray-600"
-              icon={faUserPlus}
-            />
-            <p className="font-semibold">Add {searchText.toString()} to Phonebook</p>
-          </div>
-          <div className={`border-b dark:border-gray-700 border-gray-200 mx-4`}></div>
-          <div className="px-4 overflow-y-auto max-h-[216px]">
-            {filteredPhoneNumbers.map((phoneNumber, index) => (
-              <SearchNumber
-                key={index}
-                name={phoneNumber.name}
-                number={phoneNumber.workphone}
-                callUser={() => callUser(phoneNumber.workphone)}
-                searchText={searchText}
-              />
-            ))}
-          </div>
-        </div>
-      )}
-    </>
+      <div
+        className="flex gap-5 pt-[10px] pr-8 pb-[10px] pl-7 w-full min-h-9 dark:hover:bg-gray-700 hover:bg-gray-200 cursor-pointer"
+        onClick={() => handleAddContactToPhonebook(true)}
+      >
+        <FontAwesomeIcon className="text-base dark:text-gray-50 text-gray-600" icon={faUserPlus} />
+        <p className="font-semibold">Add {searchText.toString()} to Phonebook</p>
+      </div>
+      <div className={`border-b dark:border-gray-700 border-gray-200 mx-4`}></div>
+      <div className="px-4 overflow-y-auto max-h-[216px]">
+        {filteredPhoneNumbers.map((phoneNumber, index) => (
+          <SearchNumber
+            key={index}
+            name={phoneNumber.name}
+            number={phoneNumber.workphone}
+            callUser={() => callUser(phoneNumber.workphone)}
+            searchText={searchText}
+          />
+        ))}
+      </div>
+    </div>
   )
 }
