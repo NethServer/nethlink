86664cd89fc06812b2a51310f4ff7158fa52e3bc
edoardopaolinelli <edoardo.paolinelli@gmail.com>
2024-03-06 16:42:47 +0100

    refactor to use react-hook-form

--- src/renderer/src/components/CreateSpeedDialBox.tsx
+++ src/renderer/src/components/CreateSpeedDialBox.tsx
@@ -1,9 +1,10 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Button, TextInput } from './Nethesis'
 import { faCircleNotch } from '@fortawesome/free-solid-svg-icons'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { NewContactType } from '@shared/types'
 import { t } from 'i18next'
+import { useForm, SubmitHandler } from 'react-hook-form'
 
 export interface CreateSpeedDialProps {
   onCancel: () => void
@@ -14,80 +15,81 @@ export function CreateSpeedDialBox({
   handleAddContactToSpeedDials,
   onCancel
 }: CreateSpeedDialProps) {
-  const [name, setName] = useState<string>('')
-  const [phoneNumber, setPhoneNumber] = useState<string>('')
+  const { register, watch, handleSubmit, setValue, reset } = useForm<NewContactType>()
+  const onSubmit: SubmitHandler<NewContactType> = (data) => {
+    handleSave(data)
+  }
   const [isLoading, setIsLoading] = useState<boolean>(false)
 
-  function handleSave(_event: React.MouseEvent) {
+  useEffect(() => {
+    setValue('name', '')
+    setValue('speeddial_num', '')
+  }, [])
+
+  function handleSave(data) {
     setIsLoading(true)
-    //Da aggiungere la visibility
-    handleAddContactToSpeedDials({ name: name, speeddial_num: phoneNumber })
+    handleAddContactToSpeedDials({ name: data.name, speeddial_num: data.speeddial_num })
       .catch((error) => {
         console.log(error)
       })
       .finally(() => {
         setIsLoading(false)
-        setName('')
-        setPhoneNumber('')
+        reset({
+          name: '',
+          speeddial_num: ''
+        })
       })
   }
 
   return (
-    <div className="flex flex-col gap-4 min-h-[284px]">
+    <div className="flex flex-col gap-4 min-h-[284px] relative">
       <div className="flex justify-between items-center py-1 border border-t-0 border-r-0 border-l-0 dark:border-gray-700 border-gray-200 max-h-[28px]">
         <h1 className="font-semibold dark:text-gray-50 text-gray-900">
           {t('SpeedDial.Create speed dial')}
         </h1>
       </div>
-      <label className="flex flex-col gap-2">
-        <p className="dark:text-gray-50 text-gray-900 font-semibold">{t('Phonebook.Name')}</p>
+      <form
+        className="flex flex-col gap-4"
+        onSubmit={(e) => {
+          handleSubmit(onSubmit)(e)
+        }}
+      >
         <TextInput
+          {...register('name')}
           type="text"
-          value={name}
-          onChange={(e) => {
-            setName(() => e.target.value)
-          }}
+          className="font-normal"
+          label={t('Phonebook.Name') as string}
         />
-      </label>
-      <label className="flex flex-col gap-2">
-        <p className="dark:text-gray-50 text-gray-900 font-semibold">
-          {t('Phonebook.Phone number')}
-        </p>
         <TextInput
+          {...register('speeddial_num')}
           type="tel"
-          pattern="[0-9]*"
           minLength={3}
-          value={phoneNumber}
           onChange={(e) => {
-            const cleanedValue = e.target.value.replace(/\D/g, '')
-
-            if (phoneNumber) {
-              setPhoneNumber(() => phoneNumber)
-            }
-            setPhoneNumber(cleanedValue)
+            setValue('speeddial_num', e.target.value.replace(/\D/g, ''))
           }}
+          className="font-normal"
+          label={t('Phonebook.Phone number') as string}
         />
-      </label>
-
-      <div className="flex flex-row gap-4 justify-end mt-2">
-        <Button variant="ghost" onClick={onCancel}>
-          <p className="dark:text-blue-500 text-blue-600 font-semibold">{t('Common.Cancel')}</p>
-        </Button>
-        <Button
-          className="dark:bg-blue-500 bg-blue-600 gap-3"
-          disabled={name.trim().length === 0 || phoneNumber.trim().length < 3}
-          onClick={handleSave}
-        >
-          <p className="dark:text-gray-900 text-gray-50 font-semibold">{t('SpeedDial.Create')}</p>
-          {isLoading && (
-            <FontAwesomeIcon
-              icon={faCircleNotch}
-              className="dark:text-gray-900 text-gray-50"
-              spin
-            />
-          )}
-        </Button>
-      </div>
+        <div className="absolute bottom-0 right-0 flex flex-row gap-4">
+          <Button variant="ghost" onClick={onCancel}>
+            <p className="dark:text-blue-500 text-blue-600 font-semibold">{t('Common.Cancel')}</p>
+          </Button>
+          <Button
+            type="submit"
+            className="dark:bg-blue-500 bg-blue-600 gap-3"
+            disabled={!watch('name') || !watch('speeddial_num')}
+          >
+            <p className="dark:text-gray-900 text-gray-50 font-semibold">{t('SpeedDial.Create')}</p>
+            {isLoading && (
+              <FontAwesomeIcon
+                icon={faCircleNotch}
+                className="dark:text-gray-900 text-gray-50"
+                spin
+              />
+            )}
+          </Button>
+        </div>
+      </form>
     </div>
   )
 }
