917d183d5980370fdfb18cbad5ff75e70719a7c1
edoardopaolinelli <edoardo.paolinelli@gmail.com>
2024-04-11 16:28:44 +0200

    refactor the classNames

--- src/renderer/src/components/DisplayedAccountLogin.tsx
+++ src/renderer/src/components/DisplayedAccountLogin.tsx
@@ -19,17 +19,19 @@ export function DisplayedAccountLogin({
     <div
       onClick={() => handleClick?.()}
       className={classNames(
-        'w-full flex flex-row gap-7 items-center justify-start bg-transparent h-20 rounded-lg text-gray-900 dark:text-gray-50 hover:text-gray-50 dark:hover:text-gray-50',
-        handleClick ? 'hover:bg-gray-400 dark:hover:bg-gray-700' : ''
+        'w-full flex flex-row gap-7 items-center justify-start bg-transparent h-20 rounded-lg text-gray-900 dark:text-gray-50',
+        handleClick ? 'hover:bg-gray-300 dark:hover:bg-gray-700' : ''
       )}
     >
       <div className="ml-5 w-12 h-12 rounded-full overflow-hidden">
         {imageSrc ? (
           <img src={imageSrc} />
         ) : (
-          <FontAwesomeIcon icon={faCircleUser} className="text-[48px]" />
+          <FontAwesomeIcon
+            icon={faCircleUser}
+            className="text-[48px] dark:text-gray-50 text-gray-600"
+          />
         )}
-        {/* <img src={imageSrc ?? placeholder} className={imageSrc ? '' : 'mix-blend-difference dark:mix-blend-normal'}></img> */}
       </div>
       <p className="w-[325px] truncate ">
         {account
