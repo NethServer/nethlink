44513444b10332b8a7ea162fbb147937678ffd52
edoardopaolinelli <edoardo.paolinelli@gmail.com>
2024-04-16 10:16:36 +0200

    fix a problem in the filter of the  missedCall

--- src/renderer/src/components/MissedCallsBox.tsx
+++ src/renderer/src/components/MissedCallsBox.tsx
@@ -16,7 +16,10 @@ export function MissedCallsBox({
   viewAllMissedCalls,
   handleSelectedMissedCall
 }: MissedCallsBoxProps): JSX.Element {
-  const missedCallsIn = missedCalls?.filter((call) => call.direction === 'in')
+  /* Oltre al fatto che sono le chiamate in entrate esse non devono aver avuto risposta */
+  const missedCallsIn = missedCalls?.filter(
+    (call) => call.direction === 'in' && call.disposition === 'NO ANSWER'
+  )
   const title = `${t('QueueManager.Missed calls')} (${missedCallsIn.length})`
 
   return (
