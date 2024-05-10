075e805404788ca2f6781e1d4cb164cba03f1564
therockerline <therockerline@gmail.com>
2024-05-10 19:37:12 +0200

    fix 40

--- src/renderer/src/hooks/useAccount.ts
+++ src/renderer/src/hooks/useAccount.ts
@@ -1,11 +1,19 @@
 import { Account, OperatorData, StatusTypes } from "@shared/types"
 import { useSubscriber } from "./useSubscriber"
+import { useEffect, useState } from "react"
 
 export const useAccount = () => {
   const account = useSubscriber<Account>('user')
   const operators = useSubscriber<OperatorData>('operators')
 
-  const status: StatusTypes = operators?.operators?.[account.username]?.mainPresence || account.data?.mainPresence || 'offline'
+  const [status, setStatus] = useState<StatusTypes>('offline')
+
+  useEffect(() => {
+    const _status: StatusTypes = operators?.operators?.[account.username]?.mainPresence || account.data?.mainPresence || 'offline'
+    setStatus(() => _status)
+
+  }, [account, operators])
+
 
   return {
     status
