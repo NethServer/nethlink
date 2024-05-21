067acde3dbdd3e8792d3673446b6dadf335b8d7c
therockerline <therockerline@gmail.com>
2024-05-21 16:54:56 +0200

    [wip] - status

--- src/renderer/src/hooks/useAccount.ts
+++ src/renderer/src/hooks/useAccount.ts
@@ -1,6 +1,7 @@
 import { Account, OperatorData, StatusTypes } from "@shared/types"
 import { useSubscriber } from "./useSubscriber"
 import { useEffect, useState } from "react"
+import { log } from "@shared/utils/logger"
 
 export const useAccount = () => {
   const account = useSubscriber<Account>('user')
@@ -10,7 +11,8 @@ export const useAccount = () => {
   const [isCallsEnabled, setIsCallsEnabled] = useState<boolean>(false)
 
   useEffect(() => {
-    const _status: StatusTypes = operators?.operators?.[account.username]?.mainPresence || account.data?.mainPresence || 'offline'
+    const _status: StatusTypes = operators?.operators?.[account.username]?.mainPresence || account.data?.mainPresence || status
+    log('STATUS', _status, account, operators)
     setStatus(() => _status)
     setIsCallsEnabled(() => !(_status === 'busy' || _status === 'ringing'))
 
