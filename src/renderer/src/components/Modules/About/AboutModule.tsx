e99829c05daf2251a4d891462575ed3b77b14d95
edoardopaolinelli <edoardo.paolinelli@gmail.com>
2024-07-03 10:01:05 +0200

    fix: added a margin bottom to prevent the focus on the button from being cut off in the About page

--- src/renderer/src/components/Modules/About/AboutModule.tsx
+++ src/renderer/src/components/Modules/About/AboutModule.tsx
@@ -2,9 +2,7 @@ import NethLinkLogoSimple from '../../../assets/LogoBlueSimple.svg'
 import NethLinkLogoSimpleDark from '../../../assets/LogoBlueSimpleDark.svg'
 import { t } from 'i18next'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-  faArrowUpRightFromSquare as DownloadIcon,
-} from '@fortawesome/free-solid-svg-icons'
+import { faArrowUpRightFromSquare as DownloadIcon } from '@fortawesome/free-solid-svg-icons'
 import { Button } from '@renderer/components/Nethesis'
 import { AvailableThemes, NotificationData } from '@shared/types'
 import { useStoreState } from '@renderer/store'
@@ -13,11 +11,9 @@ import { Scrollable } from '@renderer/components/Scrollable'
 import { ModuleTitle } from '@renderer/components/ModuleTitle'
 import { parseThemeToClassName } from '@renderer/utils'
 
-export interface AboutBoxProps {
+export interface AboutBoxProps {}
 
-}
-
-export function AboutModule({ }: AboutBoxProps) {
+export function AboutModule({}: AboutBoxProps) {
   const page = usePageCtx()
   const [theme] = useStoreState<AvailableThemes>('theme')
   const [notifications] = useStoreState<NotificationData>('notifications')
@@ -32,32 +28,44 @@ export function AboutModule({ }: AboutBoxProps) {
 
   return (
     <>
-      <ModuleTitle
-        title={t('About.title')}
-      />
+      <ModuleTitle title={t('About.title')} />
       <Scrollable>
-        <div className='flex flex-col w-full items-center px-4 text-bgDark dark:text-bgLight'>
-          <div className='mt-6 mb-4 flex flex-col items-center gap-2'>
-            <img src={parseThemeToClassName(theme) === 'dark' ? NethLinkLogoSimpleDark : NethLinkLogoSimple} className="h-10 overflow-hidden object-cover place-items-start"></img>
-            <div className='font-medium'>NethLink by <span className='text-textBlueLight dark:text-textBlueDark cursor-pointer' onClick={openNethesisPage}>Nethesis</span></div>
-          </div>
-          <div className='text-gray-400 mb-2'>{t('About.current_version', { version: page?.props.appVersion })}</div>
-          {
-            !!notifications?.system?.update && <div className='mt-6 flex flex-col gap-2 items-center'>
-              <span className='font-medium text-[12px] leading-[18px]'>{t('About.update_available')}</span>
-              <Button
-                variant='white'
-                onClick={onDownloadButtonClick}
+        <div className="flex flex-col w-full items-center px-4 text-bgDark dark:text-bgLight mb-1">
+          <div className="mt-6 mb-4 flex flex-col items-center gap-2">
+            <img
+              src={
+                parseThemeToClassName(theme) === 'dark'
+                  ? NethLinkLogoSimpleDark
+                  : NethLinkLogoSimple
+              }
+              className="h-10 overflow-hidden object-cover place-items-start"
+            ></img>
+            <div className="font-medium">
+              NethLink by{' '}
+              <span
+                className="text-textBlueLight dark:text-textBlueDark cursor-pointer"
+                onClick={openNethesisPage}
               >
-                <div className='flex flex-row items-center gap-4'>
+                Nethesis
+              </span>
+            </div>
+          </div>
+          <div className="text-gray-400 mb-2">
+            {t('About.current_version', { version: page?.props.appVersion })}
+          </div>
+          {!!notifications?.system?.update && (
+            <div className="mt-6 flex flex-col gap-2 items-center">
+              <span className="font-medium text-[12px] leading-[18px]">
+                {t('About.update_available')}
+              </span>
+              <Button variant="white" onClick={onDownloadButtonClick}>
+                <div className="flex flex-row items-center gap-4">
                   <FontAwesomeIcon size="1x" icon={DownloadIcon} className="text-[16px]" />
-                  <span className='font-medium text-sm'>
-                    {t('About.download')}
-                  </span>
+                  <span className="font-medium text-sm">{t('About.download')}</span>
                 </div>
               </Button>
             </div>
-          }
+          )}
         </div>
       </Scrollable>
     </>
