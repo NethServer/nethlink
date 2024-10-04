56fe88b4805c04b5ad951c8fdd74a5e61e909b37
Edoardo Spadoni <edoardo.spadoni@gmail.com>
2024-10-04 09:03:23 +0200

    Update src/main/classes/controllers/AccountController.ts

--- src/main/classes/controllers/AccountController.ts
+++ src/main/classes/controllers/AccountController.ts
@@ -80,7 +80,6 @@ export class AccountController {
           let loggedAccount: Account = {
             ...lastLoggedAccount,
             ...tempLoggedAccount,
-            //theme: lastLoggedAccount.theme 
           }
 
           const { parseConfig } = useLogin()
