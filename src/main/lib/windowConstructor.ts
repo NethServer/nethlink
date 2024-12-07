fc2cde8c263df3030ed9a6b43e0f37cab2494cc0
therockerline <therockerline@gmail.com>
2024-12-07 11:58:31 +0100

    code cleanup and log upgrade, add new env variable (isDevTools to immediatly show the dev tool when a window was open)

--- src/main/lib/windowConstructor.ts
+++ src/main/lib/windowConstructor.ts
@@ -3,6 +3,7 @@ import { BrowserWindow, ipcMain } from 'electron'
 import { mainBindings } from 'i18next-electron-fs-backend'
 import { join } from 'path'
 import fs from 'fs'
+import { isDevTools } from '@shared/utils/utils'
 
 export type WindowOptions = {
   rendererPath?: string
@@ -57,7 +58,7 @@ export function createWindow(
   mainWindow.on('close', () => { })
 
   mainWindow.on('ready-to-show', () => {
-    mainWindow.webContents.openDevTools({
+    isDevTools() && mainWindow.webContents.openDevTools({
       mode: 'detach'
     })
   })
