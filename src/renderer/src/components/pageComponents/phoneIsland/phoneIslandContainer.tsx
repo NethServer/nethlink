430f27f6188b18b5ec0007a6f7043bccbc7a8f11
therockerline <therockerline@gmail.com>
2025-02-26 18:03:03 +0100

    fix: change to new size system

--- src/renderer/src/components/pageComponents/phoneIsland/phoneIslandContainer.tsx
+++ src/renderer/src/components/pageComponents/phoneIsland/phoneIslandContainer.tsx
@@ -1,14 +1,11 @@
 import { PhoneIsland } from "@nethesis/phone-island"
-import { eventDispatch } from "@renderer/hooks/eventDispatch"
-import { useLoggedNethVoiceAPI } from "@renderer/hooks/useLoggedNethVoiceAPI"
 import { useSharedState } from "@renderer/store"
-import { IPC_EVENTS, PHONE_ISLAND_EVENTS } from "@shared/constants"
+import { IPC_EVENTS } from "@shared/constants"
 import { Log } from "@shared/utils/logger"
 import { useEffect, useMemo } from "react"
 
-export const PhoneIslandContainer = ({ dataConfig, deviceInformationObject, isDataConfigCreated, i18nLoadPath }) => {
+export const PhoneIslandContainer = ({ dataConfig, deviceInformationObject, isDataConfigCreated }) => {
   const [account] = useSharedState('account')
-  const { NethVoiceAPI } = useLoggedNethVoiceAPI()
 
   useEffect(() => {
     updateAccountInfo()
@@ -17,18 +14,14 @@ export const PhoneIslandContainer = ({ dataConfig, deviceInformationObject, isDa
   const updateAccountInfo = async () => {
     if (deviceInformationObject) {
       Log.info('FORCE DEFAULT DEVICE TO NETHLINK')
-      //TODO: controlla
-      if(account?.data?.default_device?.type === 'webrtc') {
+      if (account?.data?.default_device?.type === 'webrtc') {
         window.electron.send(IPC_EVENTS.CHANGE_DEFAULT_DEVICE, deviceInformationObject)
       }
-      // await NethVoiceAPI.User.default_device(deviceInformationObject)
-      // eventDispatch(PHONE_ISLAND_EVENTS['phone-island-default-device-change'], { deviceInformationObject })
-
     }
   }
 
   const PhoneIslandComponent = useMemo(() => {
-    return dataConfig && isDataConfigCreated && <PhoneIsland dataConfig={dataConfig} i18nLoadPath={i18nLoadPath} uaType='mobile' />
+    return dataConfig && isDataConfigCreated && <PhoneIsland dataConfig={dataConfig} uaType='mobile' />
   }, [account?.username, dataConfig, isDataConfigCreated])
 
   return PhoneIslandComponent
