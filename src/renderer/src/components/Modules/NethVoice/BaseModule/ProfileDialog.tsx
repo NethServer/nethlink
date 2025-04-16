2810ff5cb89253ae36813f303966761f84a83219
tonyco97 <tonyco97.tc@gmail.com>
2025-04-16 09:38:43 +0200

    fix: missing faAngleRight and fixed indentation

--- src/renderer/src/components/Modules/NethVoice/BaseModule/ProfileDialog.tsx
+++ src/renderer/src/components/Modules/NethVoice/BaseModule/ProfileDialog.tsx
@@ -1,7 +1,9 @@
 import {
   faXmarkCircle as ExitIcon,
+  faAngleRight,
+  faVectorSquare,
   faArrowRightFromBracket as LogoutIcon,
-  faGear as SettingsIcon
+  faGear as SettingsIcon,
 } from '@fortawesome/free-solid-svg-icons'
 import { motion } from 'motion/react'
 import { ProfileData } from './ProfileDialog/ProfileData'
@@ -18,36 +20,47 @@ import { Line } from './ProfileDialog/Line'
 import { PresenceBox } from './ProfileDialog/PresenceSettings/PresenceBox'
 import { SettingsBox } from './ProfileDialog/SettingsSettings/SettingsBox'
 import { ThemeBox, ThemeIcons } from './ProfileDialog/ThemeSettings/ThemeBox'
-import { DeviceBox, DeviceIcons } from './ProfileDialog/DeviceSettings/DeviceBox'
+import {
+  DeviceBox,
+  DeviceIcons,
+} from './ProfileDialog/DeviceSettings/DeviceBox'
 import { PresenceForwardDialog } from './ProfileDialog/PresenceSettings/PresenceForwardDialog'
 import { SettingsShortcutDialog } from './ProfileDialog/SettingsSettings/SettingsShortcutDialog'
 import { Backdrop } from './Backdrop'
 import { IconDefinition } from '@nethesis/nethesis-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 enum MenuItem {
   device = 1,
   presence,
   theme,
   settings,
-
 }
 export const ProfileDialog = ({
   isOpen,
   onClose,
 }: {
-  isOpen: boolean,
+  isOpen: boolean
   onClose: () => void
 }) => {
-  const { status, } = useAccount()
-  const [selectedMenu, setSelectedMenu] = useState<MenuItem | undefined>(undefined)
+  const { status } = useAccount()
+  const [selectedMenu, setSelectedMenu] = useState<MenuItem | undefined>(
+    undefined,
+  )
   const [account] = useSharedState('account')
   const [device] = useSharedState('device')
   const [x, setX] = useState(0)
   const [dialogPageTitle, setDialogPageTitle] = useState('')
   const [themeIcon, setThemeIcon] = useState<IconProp>(ThemeIcons['system'])
-  const [deviceIcon, setDeviceIcon] = useState<IconProp | IconDefinition | ({ Icon: JSX.Element })>(DeviceIcons['nethlink'])
-  const [isForwardDialogOpen, setIsForwardDialogOpen] = useNethlinkData('isForwardDialogOpen')
-  const [isShortcutDialogOpen, setIsShortcutDialogOpen] = useNethlinkData('isShortcutDialogOpen')
+  const [deviceIcon, setDeviceIcon] = useState<
+    IconProp | IconDefinition | { Icon: JSX.Element }
+  >(DeviceIcons['nethlink'])
+  const [isForwardDialogOpen, setIsForwardDialogOpen] = useNethlinkData(
+    'isForwardDialogOpen',
+  )
+  const [isShortcutDialogOpen, setIsShortcutDialogOpen] = useNethlinkData(
+    'isShortcutDialogOpen',
+  )
   function handleExitNethLink() {
     window.api.exitNethLink()
   }
@@ -56,10 +69,18 @@ export const ProfileDialog = ({
     if (selectedMenu) {
       setX(250)
       switch (selectedMenu) {
-        case MenuItem.device: setDialogPageTitle(() => t("TopBar.Pair device")); break;
-        case MenuItem.theme: setDialogPageTitle(() => t("TopBar.Theme")); break;
-        case MenuItem.presence: setDialogPageTitle(() => t("TopBar.Presence")); break;
-        case MenuItem.settings: setDialogPageTitle(() => t("TopBar.Settings")); break;
+        case MenuItem.device:
+          setDialogPageTitle(() => t('TopBar.Pair device'))
+          break
+        case MenuItem.theme:
+          setDialogPageTitle(() => t('TopBar.Theme'))
+          break
+        case MenuItem.presence:
+          setDialogPageTitle(() => t('TopBar.Presence'))
+          break
+        case MenuItem.settings:
+          setDialogPageTitle(() => t('TopBar.Settings'))
+          break
       }
     } else {
       setDialogPageTitle(() => '')
@@ -75,7 +96,6 @@ export const ProfileDialog = ({
     if (account) {
       setDeviceIcon(() => DeviceIcons[device?.type || 'nethlink'])
     }
-
   }, [device])
 
   useEffect(() => {
@@ -84,89 +104,111 @@ export const ProfileDialog = ({
     }
   }, [account])
   if (!isOpen) return <></>
-  return <div className={classNames('absolute')}>
-    {isForwardDialogOpen && <PresenceForwardDialog />}
-    {isShortcutDialogOpen && <SettingsShortcutDialog />}
-    <div className={
-      classNames(
-        'w-[252px] h-[333px]',
-        'bg-bgInput dark:bg-bgInputDark',
-        'rounded-lg border dark:border-borderDark border-borderLight',
-        'fixed z-[200] right-[58px] top-[54px]'
-      )
-    } >
-      <div className='relative w-full h-full overflow-hidden'>
-        <div className='flex flex-col'>
-          <ProfileData />
-          <Line />
-          <div className='py-2'>
-            <MenuAction.itemWrap onClick={() => setSelectedMenu(() => MenuItem.presence)} >
-
-              <StatusDot status={status} className='ml-1' />
-              <p className="font-normal pl-1">{t('TopBar.Presence')}</p>
-            </MenuAction.itemWrap>
-            <MenuAction.item
-              onClick={() => setSelectedMenu(() => MenuItem.device)}
-              label={t("TopBar.Pair device")}
-              {...(
-                deviceIcon?.hasOwnProperty('Icon') ? {
-                  iconElem: (deviceIcon as { Icon: JSX.Element }).Icon
-                }
+  return (
+    <div className={classNames('absolute')}>
+      {isForwardDialogOpen && <PresenceForwardDialog />}
+      {isShortcutDialogOpen && <SettingsShortcutDialog />}
+      <div
+        className={classNames(
+          'w-[252px] h-[333px]',
+          'bg-bgInput dark:bg-bgInputDark',
+          'rounded-lg border dark:border-borderDark border-borderLight',
+          'fixed z-[200] right-[58px] top-[54px]',
+        )}
+      >
+        <div className='relative w-full h-full overflow-hidden'>
+          <div className='flex flex-col'>
+            <ProfileData />
+            <Line />
+            <div className='py-2'>
+              <MenuAction.itemWrap
+                onClick={() => setSelectedMenu(() => MenuItem.presence)}
+              >
+                <StatusDot status={status} className='ml-1' />
+                <p className='font-normal pl-1'>{t('TopBar.Presence')}</p>
+                <FontAwesomeIcon
+                  icon={faAngleRight}
+                  className='ml-auto h-4 w-4 flex justify-center'
+                />
+              </MenuAction.itemWrap>
+              <MenuAction.item
+                onClick={() => setSelectedMenu(() => MenuItem.device)}
+                label={t('TopBar.Pair device')}
+                {...(deviceIcon?.hasOwnProperty('Icon')
+                  ? {
+                      iconElem: (deviceIcon as { Icon: JSX.Element }).Icon,
+                    }
                   : {
-                    icon: deviceIcon as IconProp
-                  }
-              )}
+                      icon: deviceIcon as IconProp,
+                    })}
+                chevronVisible
+              />
+              <MenuAction.item
+                onClick={() => setSelectedMenu(() => MenuItem.theme)}
+                icon={themeIcon}
+                label={t('TopBar.Theme')}
+                chevronVisible
+              />
+              <MenuAction.item
+                onClick={() => setSelectedMenu(() => MenuItem.settings)}
+                icon={SettingsIcon}
+                label={t('TopBar.Settings')}
+                chevronVisible
+              />
+            </div>
+            <Line />
+            <MenuAction.item
+              className={'py-2'}
+              onClick={handleLogout}
+              icon={LogoutIcon}
+              label={t('TopBar.Logout')}
             />
-            <MenuAction.item onClick={() => setSelectedMenu(() => MenuItem.theme)} icon={themeIcon} label={t('TopBar.Theme')} />
-            <MenuAction.item onClick={() => setSelectedMenu(() => MenuItem.settings)} icon={SettingsIcon} label={t('TopBar.Settings')} />
+            <Line />
+            <MenuAction.item
+              className={'py-2'}
+              onClick={handleExitNethLink}
+              icon={ExitIcon}
+              label={t('Common.Quit')}
+            />
           </div>
-          <Line />
-          <MenuAction.item className={'py-2'} onClick={handleLogout} icon={LogoutIcon} label={t('TopBar.Logout')} />
-          <Line />
-          <MenuAction.item className={'py-2'} onClick={handleExitNethLink} icon={ExitIcon} label={t('Common.Quit')} />
-        </div>
-        <motion.div
-          className='absolute top-0 left-full h-full w-[calc(100%+1px)]'
-          animate={{ x: -x, }}
-          transition={{ duration: 0.15, ease: "linear" }}
-        >
-          <MenuPage
-            goBack={() => setSelectedMenu(() => undefined)}
-            title={dialogPageTitle}
+          <motion.div
+            className='absolute top-0 left-full h-full w-[calc(100%+1px)]'
+            animate={{ x: -x }}
+            transition={{ duration: 0.15, ease: 'linear' }}
           >
-            {selectedMenu === MenuItem.presence && <PresenceBox />}
-            {selectedMenu === MenuItem.device && <DeviceBox />}
-            {selectedMenu === MenuItem.theme && <ThemeBox />}
-            {selectedMenu === MenuItem.settings && <SettingsBox />}
-          </MenuPage>
-
-        </motion.div>
+            <MenuPage
+              goBack={() => setSelectedMenu(() => undefined)}
+              title={dialogPageTitle}
+            >
+              {selectedMenu === MenuItem.presence && <PresenceBox />}
+              {selectedMenu === MenuItem.device && <DeviceBox />}
+              {selectedMenu === MenuItem.theme && <ThemeBox />}
+              {selectedMenu === MenuItem.settings && <SettingsBox />}
+            </MenuPage>
+          </motion.div>
+        </div>
       </div>
-    </div >
-    <Backdrop
-      className={
-        classNames(
+      <Backdrop
+        className={classNames(
           isForwardDialogOpen || isShortcutDialogOpen
             ? 'bg-white opacity-[0.25] z-[201]'
-            : 'z-[199]'
-        )
-      }
-      onBackdropClick={() => {
-        if (isForwardDialogOpen) {
-          setIsForwardDialogOpen(() => false)
-        } else {
-          setSelectedMenu(() => undefined)
-          onClose()
-        }
-        if (isShortcutDialogOpen) {
-          setIsShortcutDialogOpen(() => false)
-        } else {
-          setSelectedMenu(() => undefined)
-          onClose()
-        }
-
-      }}
-    />
-  </div>
-
+            : 'z-[199]',
+        )}
+        onBackdropClick={() => {
+          if (isForwardDialogOpen) {
+            setIsForwardDialogOpen(() => false)
+          } else {
+            setSelectedMenu(() => undefined)
+            onClose()
+          }
+          if (isShortcutDialogOpen) {
+            setIsShortcutDialogOpen(() => false)
+          } else {
+            setSelectedMenu(() => undefined)
+            onClose()
+          }
+        }}
+      />
+    </div>
+  )
 }
