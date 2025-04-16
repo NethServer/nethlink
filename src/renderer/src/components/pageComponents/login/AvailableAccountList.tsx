e62cf743a461de753574f680691073feb4dc6847
tonyco97 <tonyco97.tc@gmail.com>
2025-04-16 15:21:48 +0200

    fix: divider UI

--- src/renderer/src/components/pageComponents/login/AvailableAccountList.tsx
+++ src/renderer/src/components/pageComponents/login/AvailableAccountList.tsx
@@ -1,10 +1,14 @@
-import { useLoginPageData, useSharedState } from "@renderer/store"
-import { Account } from "@shared/types"
-import { t } from "i18next"
-import { DisplayedAccountLogin } from "./DisplayedAccountLogin"
-import { NEW_ACCOUNT } from "@shared/constants"
+import { useLoginPageData, useSharedState } from '@renderer/store'
+import { Account } from '@shared/types'
+import { t } from 'i18next'
+import { DisplayedAccountLogin } from './DisplayedAccountLogin'
+import { NEW_ACCOUNT } from '@shared/constants'
 
-export const AvailableAccountList = ({ handleDeleteAccount }: { handleDeleteAccount: (a: Account) => void }) => {
+export const AvailableAccountList = ({
+  handleDeleteAccount,
+}: {
+  handleDeleteAccount: (a: Account) => void
+}) => {
   const [auth] = useSharedState('auth')
   const [, setSelectedAccount] = useLoginPageData('selectedAccount')
   const handleSelectAccount = (account) => {
@@ -12,14 +16,14 @@ export const AvailableAccountList = ({ handleDeleteAccount }: { handleDeleteAcco
   }
 
   return (
-    <div className="w-full mt-7">
-      <p className="text-titleLight dark:text-titleDark text-[20px] leading-[30px] font-medium mb-2">
+    <div className='w-full mt-7'>
+      <p className='text-titleLight dark:text-titleDark text-[20px] leading-[30px] font-medium mb-2'>
         {t('Login.Account List title')}
       </p>
-      <p className="text-titleLight dark:text-titleDark text-[14px] leading-5 mb-7">
+      <p className='text-titleLight dark:text-titleDark text-[14px] leading-5 mb-7'>
         {t('Login.Account List description')}
       </p>
-      <div className="max-h-60 overflow-y-auto">
+      <div className='max-h-60 overflow-y-auto'>
         {Object.values(auth!.availableAccounts)!.map((account, idx) => {
           return (
             <DisplayedAccountLogin
@@ -32,13 +36,16 @@ export const AvailableAccountList = ({ handleDeleteAccount }: { handleDeleteAcco
           )
         })}
       </div>
-      <div className="flex items-center">
-        <div className="flex-grow border-t border-gray-300 dark:border-gray-600"></div>
-        <span className="mx-4 text-gray-500 dark:text-gray-400 text-sm">{t('Common.or')}</span>
-        <div className="flex-grow border-t border-gray-300 dark:border-gray-600"></div>
+      <div className='flex items-center'>
+        <div className='flex-grow border-b dark:border-borderDark border-borderLight'></div>
+        <span className='mx-4 text-titleLight dark:text-titleDark text-sm'>
+          {t('Common.or')}
+        </span>
+        <div className='flex-grow border-b dark:border-borderDark border-borderLight'></div>
       </div>
-      <DisplayedAccountLogin handleClick={() => handleSelectAccount(NEW_ACCOUNT)} />
+      <DisplayedAccountLogin
+        handleClick={() => handleSelectAccount(NEW_ACCOUNT)}
+      />
     </div>
   )
 }
-
