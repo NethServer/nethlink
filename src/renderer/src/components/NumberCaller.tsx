10af7f9c49285a348a55f45fc73c78cf4b45cb8e
tonyco97 <tonyco97.tc@gmail.com>
2025-06-20 15:44:11 +0200

    fix: missing call without dev mode

--- src/renderer/src/components/NumberCaller.tsx
+++ src/renderer/src/components/NumberCaller.tsx
@@ -1,6 +1,5 @@
 import { ClassNames } from '@renderer/utils'
 import { ReactNode } from 'react'
-
 type NumberCallerProps = {
   number: number | string
   children: JSX.Element | JSX.Element[] | ReactNode | ReactNode[]
@@ -16,26 +15,20 @@ export const NumberCaller = ({
   isNumberHiglighted = true,
   ...args
 }: NumberCallerProps) => {
-  const onClick = () => {
-    const url = `callto://${('' + number).replace(/ /g, '')}`
-    window.api.openExternalPage(url);
-  }
 
   return disabled ? (
     <div className={ClassNames(className, 'cursor-not-allowed',)}>{children}</div>
   ) : (
-    <div
-      className={ClassNames(className,
-        'dark:text-textBlueDark text-textBlueLight',
-        'cursor-pointer dark:focus:outline-none dark:focus:ring-2 focus:outline-none focus:ring-2 dark:ring-offset-1 ring-offset-1 dark:ring-offset-slate-900 ring-offset-slate-50 focus:ring-primaryRing dark:focus:ring-primaryRingDark rounded-md')}
+    <a
+      href={`callto://${('' + number).replace(/ /g, '')}`}
+      className={ClassNames(className, 'dark:focus:outline-none dark:focus:ring-2 focus:outline-none focus:ring-2 dark:ring-offset-1 ring-offset-1 dark:ring-offset-slate-900 ring-offset-slate-50 focus:ring-primaryRing dark:focus:ring-primaryRingDark rounded-md')}
       {...args}
-      onClick={onClick}
     >
       <div
-        className={`${isNumberHiglighted ? 'dark:text-textBlueDark text-textBlueLight' : ''} font-normal`}
+        className={`${isNumberHiglighted ? 'dark:text-titleDark text-titleLight' : ''} font-normal`}
       >
         {children}
       </div>
-    </div>
+    </a>
   )
 }
