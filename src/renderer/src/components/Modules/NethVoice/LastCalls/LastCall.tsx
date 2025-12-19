15ea441cff586cc8e92345a779a99cb92f166c94
Tommaso Ascani <tommaso.ascani@gmail.com>
2025-12-19 11:38:12 +0100

    refactor: improve formatting and structure in LastCall and CallsDate components

--- src/renderer/src/components/Modules/NethVoice/LastCalls/LastCall.tsx
+++ src/renderer/src/components/Modules/NethVoice/LastCalls/LastCall.tsx
@@ -1,15 +1,13 @@
 import {
   faUserPlus as AddUserIcon,
   faUsers as BadgeIcon,
-  faCircleUser
+  faCircleUser,
 } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Avatar, Button } from '../../../Nethesis'
 import { NumberCaller } from '../../../NumberCaller'
 import { useEffect, useState } from 'react'
-import {
-  LastCallData,
-} from '@shared/types'
+import { LastCallData } from '@shared/types'
 import { t } from 'i18next'
 import { CallsDate } from '../../../Nethesis/CallsDate'
 import { truncate } from '@renderer/utils'
@@ -22,7 +20,7 @@ import {
   OutCallAnsweredIcon,
   OutCallNotAnsweredIcon,
   InCallAnsweredIcon,
-  InCallNotAnsweredIcon
+  InCallNotAnsweredIcon,
 } from '@renderer/icons'
 import classNames from 'classnames'
 
@@ -37,7 +35,7 @@ export function LastCall({
   call,
   showContactForm,
   clearNotification,
-  className
+  className,
 }: LastCallProps): JSX.Element {
   const phonebookModule = usePhonebookModule()
   const [, setSelectedContact] = phonebookModule.selectedContact
@@ -49,13 +47,15 @@ export function LastCall({
   const avatarSrc = operators?.avatars?.[call.username]
 
   function getOperatorByPhoneNumber(phoneNumber: string, operators: any) {
-    return Object.values(operators).find((extensions: any) => extensions.id === phoneNumber)
+    return Object.values(operators).find(
+      (extensions: any) => extensions.id === phoneNumber,
+    )
   }
 
   if (call?.dst_cnam === '') {
     const operatorFound: any = getOperatorByPhoneNumber(
       call?.dst as string,
-      operators?.operators || {}
+      operators?.operators || {},
     )
 
     if (operatorFound) {
@@ -69,7 +69,10 @@ export function LastCall({
     }
   }, [queues])
 
-  const handleSelectedCallContact = (number: string, company: string | undefined) => {
+  const handleSelectedCallContact = (
+    number: string,
+    company: string | undefined,
+  ) => {
     if (company === undefined) {
       setSelectedContact({ number, company: '' })
     } else setSelectedContact({ number, company })
@@ -82,26 +85,28 @@ export function LastCall({
         ? call?.cnam || call?.ccompany
         : call.direction === 'out'
           ? call?.dst_cnam || call?.dst_ccompany
-          : undefined
+          : undefined,
     )
     showContactForm()
   }
 
   const getCallName = (call: LastCallData) => {
-    return `${call.direction === 'in'
-      ? (call.cnam || call.ccompany || t('Common.Unknown'))
-      : call.direction === 'out'
-        ? (call.dst_cnam || call.dst_ccompany || t('Common.Unknown'))
-        : t('Common.Unknown')
-      }`
+    return `${
+      call.direction === 'in'
+        ? call.cnam || call.ccompany || t('Common.Unknown')
+        : call.direction === 'out'
+          ? call.dst_cnam || call.dst_ccompany || t('Common.Unknown')
+          : t('Common.Unknown')
+    }`
   }
 
   const tooltipId = call.uniqueid?.replace('.', '_')
+  const isFromQueue = Boolean(call.channel?.includes('from-queue'))
 
   return (
-    <div className="group">
+    <div className='group'>
       <div
-        className={`flex flex-grow gap-3 min-h-[72px] p-2 ${className}`}
+        className={`flex flex-grow gap-3 min-h-[72px] py-6 px-3 ${className}`}
         onMouseEnter={() => {
           if (
             call.direction === 'in'
@@ -125,44 +130,91 @@ export function LastCall({
             />
           </div>
         )}
-        <div className="flex flex-col h-full min-w-6 pt-[6px] z-10">
+        <div className='flex flex-col h-full min-w-6 pt-[6px] z-10'>
           {avatarSrc ? (
             <Avatar
-              size="small"
+              size='small'
               src={avatarSrc}
               status={operators?.operators?.[call.username]?.mainPresence}
             />
           ) : (
             <FontAwesomeIcon
               icon={faCircleUser}
-              className="h-8 w-8 dark:text-gray-200 text-gray-400"
+              className='h-8 w-8 dark:text-gray-200 text-gray-400'
             />
           )}
         </div>
-        <div
-          className="flex flex-col gap-1 dark:text-titleDark text-titleLight"
-        >
-          <p className={`font-medium text-[14px] leading-5`}
+        <div className='flex flex-col gap-1 dark:text-titleDark text-titleLight'>
+          <p
+            className={`font-medium text-[14px] leading-5`}
             data-tooltip-id={`tooltip-username-${tooltipId}`}
             data-tooltip-content={getCallName(call)}
           >
-            {truncate(getCallName(call), call.channel?.includes('from-queue') ? 13 : Infinity)}
+            {truncate(getCallName(call), 24)}
           </p>
-          <Tooltip id={`tooltip-username-${tooltipId}`} place='bottom' opacity={1} className="z-10" />
+          <Tooltip
+            id={`tooltip-username-${tooltipId}`}
+            place='bottom'
+            opacity={1}
+            className='z-10'
+          />
+
+          {isFromQueue && (
+            <div>
+              {isQueueLoading ? (
+                <Badge
+                  variant='offline'
+                  rounded='full'
+                  className={`animate-pulse overflow-hidden w-[108px] min-h-4`}
+                ></Badge>
+              ) : (
+                <Badge
+                  size='small'
+                  variant='offline'
+                  rounded='full'
+                  className={`overflow-hidden`}
+                  data-tooltip-id={`tooltip-queue-${tooltipId}`}
+                  data-tooltip-content={
+                    queues?.[call.queue!]?.name
+                      ? queues?.[call.queue!]?.name + ' ' + call?.queue
+                      : t('QueueManager.Queue')
+                  }
+                >
+                  <FontAwesomeIcon
+                    icon={BadgeIcon}
+                    className='h-4 w-4 mr-2 ml-1'
+                    aria-hidden='true'
+                  />
+                  <div className='truncate max-w-48 mr-1'>
+                    {queues?.[call.queue!]?.name
+                      ? queues?.[call.queue!]?.name + ' ' + call?.queue
+                      : t('QueueManager.Queue')}
+                  </div>
+                  <Tooltip
+                    id={`tooltip-queue-${tooltipId}`}
+                    place='bottom'
+                    className='z-[10]'
+                    opacity={1}
+                  />
+                </Badge>
+              )}
+            </div>
+          )}
+
           <div
-            className="flex flex-row gap-2 items-center"
+            className='flex flex-row gap-2 items-center'
             data-tooltip-id={`call_${tooltipId}`}
-            data-tooltip-content={call.disposition === 'NO ANSWER'
-              ? call.direction === 'in'
-                ? t('History.Incoming missed')
-                : t('History.Outgoing missed')
-              : call.direction === 'in'
-                ? t('History.Incoming answered')
-                : t('History.Outgoing answered')}
+            data-tooltip-content={
+              call.disposition === 'NO ANSWER'
+                ? call.direction === 'in'
+                  ? t('History.Incoming missed')
+                  : t('History.Outgoing missed')
+                : call.direction === 'in'
+                  ? t('History.Incoming answered')
+                  : t('History.Outgoing answered')
+            }
           >
-            <div
-              className={`h-4 w-4`}
-            >
+            <div className={`h-4 w-4`}>
               {call.disposition === 'NO ANSWER' ? (
                 call.direction === 'in' ? (
                   <InCallNotAnsweredIcon />
@@ -177,83 +229,42 @@ export function LastCall({
             </div>
             <Tooltip
               id={`call_${tooltipId}`}
-              place="right"
-              className="z-10"
+              place='right'
+              className='z-10'
               opacity={1}
               noArrow={false}
             />
             <NumberCaller
-              number={(call.direction === 'in' ? call.src : call.dst) || 'no number'}
+              number={
+                (call.direction === 'in' ? call.src : call.dst) || 'no number'
+              }
               disabled={!isCallsEnabled}
               className={
                 'dark:text-textBlueDark text-textBlueLight font-normal text-[14px] leading-5 hover:underline inset-pink-600'
               }
               isNumberHiglighted={false}
             >
-              {(call.direction === 'in' ? call.src : call.dst) || t('Common.Unknown')}
+              {(call.direction === 'in' ? call.src : call.dst) ||
+                t('Common.Unknown')}
             </NumberCaller>
           </div>
-          <div className="flex flex-row gap-1">
-            <CallsDate call={call} spaced={true} />
-          </div>
+
+          <CallsDate call={call} inline={true} />
         </div>
 
-        <div className="flex flex-col justify-between ml-auto items-center">
-          {call.channel?.includes('from-queue') && (
-            <div className="absolute w-0 overflow-visible flex flex-row-reverse ">
-              <div className="relative right-[-16px]">
-                {isQueueLoading ? (
-                  <Badge
-                    variant="offline"
-                    rounded="full"
-                    className={`animate-pulse overflow-hidden ml-1 w-[108px] min-h-4`}
-                  ></Badge>
-                ) : (
-                  <Badge
-                    size="small"
-                    variant="offline"
-                    rounded="full"
-                    className={`overflow-hidden ml-1 `}
-                    data-tooltip-id={`tooltip-queue-${tooltipId}`}
-                    data-tooltip-content={queues?.[call.queue!]?.name
-                      ? queues?.[call.queue!]?.name + ' ' + call?.queue
-                      : t('QueueManager.Queue')}
-                  >
-                    <FontAwesomeIcon
-                      icon={BadgeIcon}
-                      className="h-4 w-4 mr-2 ml-1"
-                      aria-hidden="true"
-                    />
-                    <div className={`truncate ${call?.queue ? 'w-20 lg:w-16 xl:w-20' : ''}`}>
-                      {truncate(
-                        queues?.[call.queue!]?.name
-                          ? queues?.[call.queue!]?.name + ' ' + call?.queue
-                          : t('QueueManager.Queue'),
-                        15
-                      )}
-                    </div>
-                    <Tooltip id={`tooltip-queue-${tooltipId}`} place='bottom' className='z-[10]' opacity={1} />
-                  </Badge>
-                )}
-              </div>
-            </div>
-          )}
-
+        <div className='flex flex-col justify-between ml-auto items-center'>
           {showCreateButton && (
-            <div className={classNames('flex relative right-[-16px] h-full items-center')}>
+            <div className={classNames('flex relative right-[-8px] h-full')}>
               <div className={classNames('absolute right-0')}>
                 <Button
-                  variant="ghost"
-                  className="flex gap-3 items-center py-2 px-3 border dark:border-borderDark border-borderLight ml-auto dark:hover:bg-hoverDark hover:bg-hoverLight"
+                  variant='ghost'
+                  className='flex gap-3 items-center py-2 px-2 border dark:border-borderDark border-borderLight ml-auto dark:hover:bg-hoverDark hover:bg-hoverLight'
                   onClick={handleCreateContact}
                 >
                   <FontAwesomeIcon
-                    className="text-base dark:text-textBlueDark text-textBlueLight"
+                    className='text-base dark:text-textBlueDark text-textBlueLight'
                     icon={AddUserIcon}
                   />
-                  <p className="dark:text-textBlueDark text-textBlueLight font-medium">
-                    {t('SpeedDial.Create')}
-                  </p>
                 </Button>
               </div>
             </div>
